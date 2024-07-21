821719b3fa4121d7a2cc5047098ca5f37f4cdd7b
otcroz <dbtndus0527@gmail.com>
2024-07-21 20:06:09 +0900

    [design] Add a code, __name__ == '__main__'
    Github issue #4

--- mongoDB.py
+++ mongoDB.py
@@ -6,52 +6,54 @@ import numpy as np
 
 load_dotenv()
 
-try:
-    # mongoDB 연결
-    uri = os.getenv("MONGO_URI")
-    client = MongoClient(uri)
 
-    # db 연결
-    database = client.test
+if __name__ == '__main__':
+    try:
+        # mongoDB 연결
+        uri = os.getenv("MONGO_URI")
+        client = MongoClient(uri)
 
-    collections = database.list_collection_names()
+        # db 연결
+        database = client.test
 
-    print(collections)
-    coll_review = database["reviews"]
-    coll_book = database["books"]
+        collections = database.list_collection_names()
 
-    # start example code here
-    proj_review = {
-        "content": 1,
-        "userId": 1,
-        "isbn": 1,
-        "evaluation": 1,
-        "_id": 0
-    }
+        print(collections)
+        coll_review = database["reviews"]
+        coll_book = database["books"]
 
-    proj_book = {
-        "title": 1,
-        "isbn": 1,
-        "writer": 1,
-        "category": 1,
-        "field": 1,
-        "_id": 0
-    }
+        # start example code here
+        proj_review = {
+            "content": 1,
+            "userId": 1,
+            "isbn": 1,
+            "evaluation": 1,
+            "_id": 0
+        }
 
-    res_review = list(coll_review.find({}, proj_review))
-    #print(res_review)
+        proj_book = {
+            "title": 1,
+            "isbn": 1,
+            "writer": 1,
+            "category": 1,
+            "field": 1,
+            "_id": 0
+        }
 
-    res_book = list(coll_book.find({}, proj_book))
-    #print(res_book)
+        res_review = list(coll_review.find({}, proj_review))
+        #print(res_review)
 
-    df_book = pd.DataFrame(res_book)
-    df_review = pd.DataFrame(res_review)
-    df_book.to_csv('book.csv', encoding='utf-8-sig')
-    df_review.to_csv('review.csv', encoding='utf-8-sig')
+        res_book = list(coll_book.find({}, proj_book))
+        #print(res_book)
 
-    # end example code here
-    client.close()
+        df_book = pd.DataFrame(res_book)
+        df_review = pd.DataFrame(res_review)
+        df_book.to_csv('book.csv', encoding='utf-8-sig')
+        df_review.to_csv('review.csv', encoding='utf-8-sig')
 
-except Exception as e:
-    raise Exception(
-        "The following error occurred: ", e)
+        # end example code here
+        client.close()
+
+    except Exception as e:
+        raise Exception(
+            "The following error occurred: ", e)
