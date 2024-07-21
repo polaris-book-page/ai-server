7cc2e956ee814440bdc03ecb98ff8beb249e8f49
otcroz <dbtndus0527@gmail.com>
2024-07-21 20:14:25 +0900

    [init/feat] Implement a flask server
    Github issue #4

--- main.py
+++ main.py
@@ -1,34 +1,15 @@
 import os
 
-from openai import OpenAI
 from dotenv import load_dotenv
+from flask import Flask
 
 load_dotenv()
 
-client = OpenAI(
-    # This is the default and can be omitted
-    api_key=os.getenv("OPEN_API_KEY"),
-)
+app = Flask(__name__)
 
-# model
-model = "gpt-3.5-turbo"
+@app.route("/")
+def test():
+    return "hello world!"
 
-# query, message
-user_category = "로맨스"
-messages = [
-    {"role": "system", "content": "You should help recommend books."},
-    {"role": "system", "content": "You must answer in Korean."},
-    {"role": "user", "content":  user_category + "책 4권 추천해줘. 이전에 추천한 책은 추천하지마. 최근 책으로 추천해줘"},
-    {"role": "assistant", "content":  "책 이름만 써줘. 이전에 추천한 책은 추천하지마."},
-]
-
-# response
-response = client.chat.completions.create(
-    model=model,
-    messages=messages,
-    temperature=0.2,
-)
-
-# answer
-answer = response.choices[0].message.content
-print(answer)
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
